720117d081ba51584a3fb7494578f490a5ed0c82
akbar <akbarbintang881@gmail.com>
2022-05-30 08:58:41 +0700

    menambahkan file baru

---
+++ latihan.php
@@ -0,0 +1,27 @@
+<!-- php : server side(mengelola) -->
+<?php
+//menampilkan data ke layar
+echo "Hello World";
+//membuat variable
+//1. diawali dengan simbol dollar
+//2. tidak boleh di awali dengan ankga dan tidak boleh spasi, spasi boleh memakai(_)
+$nama = "Eko";
+echo $nama;
+var_dump($nama);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba PHP</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
